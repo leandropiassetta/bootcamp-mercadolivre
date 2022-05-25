9686fa78bad1aac6200940d5b7174edab4d2790f
leandropiassetta <leandropiassetta@gmail.com>
2022-05-24 23:21:39 -0300

    gobases2: exerciciosManha: ex5

--- bootcamp/gobases2/exerciciosManha/ex5/ex.go
+++ bootcamp/gobases2/exerciciosManha/ex5/ex.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	dog       = "dog"
+	cat       = "cat"
+	hamster   = "hamster"
+	tarantula = "tarantula"
+)
+
+//dogFunc returns in grams
+func dogFood(quantity int) int {
+	return quantity * 10000
+}
+
+//catFunc returns in grams
+func catFood(quantity int) int {
+	return quantity * 5000
+}
+
+//hamsterFunc returns in grams
+func hamsterFood(quantity int) int {
+	return quantity * 250
+}
+
+//tarantulaFunc returns in grams
+func tarantulaFood(quantity int) int {
+	return quantity * 150
+}
+
+//Animal returns a func for a specific animal
+func Animal(specie string) (func(quantity int) int, error) {
+	if specie == dog {
+		return dogFood, nil
+
+	} else if specie == cat {
+		return catFood, nil
+
+	} else if specie == hamster {
+		return hamsterFood, nil
+
+	} else if specie == tarantula {
+		return tarantulaFood, nil
+
+	} else {
+		return nil, errors.New("invalid animal type")
+	}
+}
 
 func main() {
-	fmt.Println("oi")
+
+	dogFunc, _ := Animal("dog")
+	catFunc, _ := Animal(cat)
+	tarantulaFunc, _ := Animal(tarantula)
+	hamsterFunc, _ := Animal(hamster)
+
+	fmt.Printf("A quantidade de alimento do(s) cachorro(s) (em gramas): %d gramas\n", dogFunc(5))
+	fmt.Printf("A quantidade de alimento do(s) gatos(s) (em gramas): %d gramas\n", catFunc(2))
+	fmt.Printf("A quantidade de alimento da(s) tarantulas(s) (em gramas): %d gramas\n", tarantulaFunc(3))
+	fmt.Printf("A quantidade de alimento do(s) hamster(s) (em gramas): %d gramas\n", hamsterFunc(4))
+
+	_, err := Animal("Urubu")
+	if err != nil {
+		fmt.Println("animal inválido")
+	}
+
 }
