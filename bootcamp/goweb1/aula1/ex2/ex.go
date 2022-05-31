857cf0a0edc7a73ee40fea8c5e0210bfd03f5eff
leandropiassetta <leandropiassetta@gmail.com>
2022-05-31 19:54:03 -0300

    feat: goweb1/aula1/ex1 done!

---
+++ bootcamp/goweb1/aula1/ex2/ex.go
@@ -0,0 +1,15 @@
+package main
+
+type product struct {
+	id           int
+	name         string
+	color        string
+	price        float64
+	code         string
+	published    bool
+	creationDate string
+}
+
+func main() {
+
+}
