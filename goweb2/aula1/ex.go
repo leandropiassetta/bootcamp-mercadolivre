4873fbe1f3508916db107d06252fd9f7c183d8f5
leandropiassetta <leandropiassetta@gmail.com>
2022-06-03 15:38:46 -0300

    feat: goweb2/aula1/ex2  done!

--- goweb2/aula1/ex.go
+++ goweb2/aula1/ex.go
@@ -1,30 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 type product struct {
 	ID       int     `json:"id"`
-	Name     string  `json:"name"`
-	Price    float64 `json:"price"`
-	Quantity int     `json:"quantity"`
+	Name     string  `json:"name" binding:"required"`
+	Price    float64 `json:"price" binding:"required"`
+	Quantity int     `json:"quantity" binding:"required"`
 }
 
 var products = []product{}
 
 func saveProducts() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// token := c.GetHeader("token")
+
+		// if token != "123456" {
+		// 	c.JSON(http.StatusUnauthorized, gin.H{
+		// 		"error": "token inválido",
+		// 	})
+		// }
+
 		var req product
 
 		if err := c.ShouldBindJSON(&req); err != nil {
+
+			validationString := "Error:Field validation"
+			errorMessage := err.Error()
+
+			if strings.Contains(errorMessage, validationString) {
+				//acha o indice inicial da string
+				errorIndex := strings.Index(errorMessage, validationString)
+
+				// recortar a string para recuperar o nome do campo
+				field := errorMessage[14 : errorIndex-2]
+
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": fmt.Sprintf("O campo %s é obrigatório", strings.ToLower(field)),
+				})
+				return
+			}
+
 			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
+				"error": errorMessage,
 			})
 			return
 		}
+
 		req.ID = len(products) + 1
 		products = append(products, req)
 		c.JSON(http.StatusOK, req)
